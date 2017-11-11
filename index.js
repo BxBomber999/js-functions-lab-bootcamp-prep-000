623697e4967874ed333f95f01da675b5ae61a64e
Peter Coyle <coyle.peters@gmail.com>
2017-11-11 18:08:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
-function happtHolidays(){
+function happyHolidays(){
   return "Happy holidays!"
 }
 
 function happyHolidaysTo(name){
-  return "Happy holidays, ${name}!"
+  return 'Happy holidays, ${name}!'
 }
 
 function happyHolidayTo(holiday,name) {
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday,name) {
 }
 
 function holidayCountdown(holiday,days){
-  return "It's ${days} days until ${holiday}!"
+  return "It's ${days} days until ${holiday}!""
 }
